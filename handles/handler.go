8f4d80e6d51ecbe7f878538ffd0bf87857b194e7
louisevanderlith <louisevanderlith@gmail.com>
2020-10-01 13:41:17 +0200

    Fixing Consent

--- handles/handler.go
+++ handles/handler.go
@@ -2,21 +2,21 @@ package handles
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/louisevanderlith/kong"
+	"github.com/louisevanderlith/kong/middle"
 	"github.com/rs/cors"
 	"net/http"
 )
 
 func SetupRoutes(scrt, securityUrl, managerUrl string) http.Handler {
 	r := mux.NewRouter()
-	ins := kong.NewResourceInspector(http.DefaultClient, securityUrl, managerUrl)
+	ins := middle.NewResourceInspector(http.DefaultClient, securityUrl, managerUrl)
 	view := ins.Middleware("logbook.history.view", scrt, ViewHistory)
 	r.HandleFunc("/history/{key:[0-9]+\\x60[0-9]+}", view).Methods(http.MethodGet)
 
 	create := ins.Middleware("logbook.history.create", scrt, CreateHistory)
 	r.HandleFunc("/history", create).Methods(http.MethodPost)
 
-	lst, err := kong.Whitelist(http.DefaultClient, securityUrl, "logbook.history.view", scrt)
+	lst, err := middle.Whitelist(http.DefaultClient, securityUrl, "logbook.history.view", scrt)
 
 	if err != nil {
 		panic(err)
